0e38a8b4460ccd2432e9dfab6a668af06a28d2ac
Nelson Chinedu <egwuchinedu69@gmail.com>
2023-03-21 08:08:54 +0100

    fix onboarding subscription

--- src/modules/Student/pages/Onboarding/index.tsx
+++ src/modules/Student/pages/Onboarding/index.tsx
@@ -1,4 +1,4 @@
-import { FunctionComponent, useState } from 'react';
+import { FunctionComponent, useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import Box from '@mui/material/Box';
@@ -11,22 +11,67 @@ import Loader from 'src/assets/images/shape-loader.gif';
 
 import MentorList from 'src/modules/Student/pages/Onboarding/MentorList';
 
-import { useVerifyPaymentQuery } from 'src/modules/Student/services/studentSlice';
+import {
+  useSubscribeMutation,
+  useVerifyPaymentQuery,
+} from 'src/modules/Student/services/studentSlice';
 
 import useLocalStorage from 'src/hooks/useLocalStorage';
+import {
+  errorNotification,
+  successNotification,
+} from 'src/helpers/notification';
+import useModal from 'src/hooks/useModal';
 
 const Onboarding: FunctionComponent<Record<never, string>> = () => {
   const navigate = useNavigate();
+  const [, setMentor] = useLocalStorage('cmid', '');
+  const [modal, _] = useModal();
   const [isSettingUp, setIsSettingUp] = useState(false);
   const [referenceId, setReferenceId] = useState('');
   const { userId } = useSelector((state: RootState) => state.account);
   const [, setStoredIsSubscribed] = useLocalStorage('csu', false);
-  const { data, isSuccess } = useVerifyPaymentQuery(
+  const { data } = useVerifyPaymentQuery(
     { reference: referenceId, userId },
     {
       skip: !referenceId,
     }
   );
+  const [subscribe] = useSubscribeMutation();
+
+  useEffect(() => {
+    if (modal.modalName) {
+      setMentor(modal?.data?.id);
+    }
+  }, [modal]);
+
+  useEffect(() => {
+    const subscription = async () => {
+      const payload = {
+        card: JSON.stringify(data?.payload),
+        mentorId: JSON.parse(localStorage.getItem('cmid') as string),
+      };
+
+      try {
+        const res =
+          referenceId && (await subscribe({ userId, data: payload }).unwrap());
+
+        if (res) {
+          setReferenceId('');
+          setTimeout(() => {
+            localStorage.removeItem('cmid');
+            setStoredIsSubscribed(true);
+            setIsSettingUp(false);
+            navigate('/s/dashboard');
+            successNotification(res?.message);
+          }, 10000);
+        }
+      } catch (error) {
+        errorNotification('An error occurred, Please try again');
+      }
+    };
+    subscription();
+  }, [data]);
 
   const onSuccess = (res: any) => {
     if (res) {
@@ -39,14 +84,6 @@ const Onboarding: FunctionComponent<Record<never, string>> = () => {
     // eslint-disable-next-line no-console
     console.log('closed');
   };
-
-  if (data && isSuccess) {
-    setTimeout(() => {
-      setStoredIsSubscribed(true);
-      setIsSettingUp(false);
-      navigate('/s/dashboard');
-    }, 10000);
-  }
 
   if (isSettingUp) {
     return (
