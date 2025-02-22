53016ed4e0e8c48c9bdfb0c8caae0029633c0389
Nelson-Chinedu <egwuchinedu69@gmail.com>
2025-02-22 17:51:30 +0100

    fix: string quote

---
+++ src/modules/Student/components/Modals/styled.viewMentorModal.ts
@@ -0,0 +1,32 @@
+import { makeStyles } from '@mui/styles';
+
+import { pxToRem } from 'src/helpers/formatFont';
+
+export const useStyles = makeStyles({
+  root: {
+    '& .MuiTypography-subtitle2': {
+      paddingBottom: '1em',
+    },
+    '& .MuiTypography-body2': {
+      fontWeight: 600,
+    },
+    '& .MuiTypography-h3': {
+      fontSize: pxToRem(20),
+      fontWeight: 600,
+      margin: '1em .6em',
+    },
+    '& .MuiButton-contained': {
+      margin: '1em 0px',
+    },
+  },
+  contentWrapper: {
+    lineHeight: '1.8em',
+    fontFamily: '\'Work Sans\', sans-serif',
+    fontWeight: 300,
+    fontSize: '14px',
+    marginLeft: '1em',
+    '& h1, h2, h3, h4, h5, h6': {
+      margin: '.8em 0px',
+    },
+  },
+});
